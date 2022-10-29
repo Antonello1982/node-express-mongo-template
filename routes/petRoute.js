959b83fd292c3a50835270bc2c9bd7c11597483e
Antonello1982 <acom1982@gmail.com>
2022-10-28 19:39:08 -0500

    Tempate backend 100%

---
+++ routes/petRoute.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PetService = require('../services/petService');
+
+const petAPI = (app) => {
+    const route = express.Router();
+    const petService = new PetService()
+
+    app.use('/api', route);
+
+    route.get('/getPets', async (req, res) => {
+        const result = await petService.getPets();
+        res.send(result)
+
+    })
+    route.get('/getSpeciesDetails', async (req, res) => {
+        const result = await petService.getSpeciesPets();
+        res.send(result);
+    })
+
+    route.post('/insertPet', async (req, res) => {
+        const result = await petService.insertPet();
+        res.send(result)
+    })
+
+}
+module.exports = petAPI;
